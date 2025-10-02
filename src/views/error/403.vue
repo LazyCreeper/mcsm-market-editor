5d36eb36e6eab1833e297c51af9e0eac1d81d929
Lazy <2211717435@qq.com>
2025-10-03 03:32:00 +0800

    opt: err page

--- src/views/error/403.vue
+++ src/views/error/403.vue
@@ -1,1 +1,14 @@
-<template><div>403</div></template>
+<script setup lang="ts">
+import err403 from '@/assets/403 Forbidden.png'
+import { t } from '@/utils/i18n'
+import { HomeOutlined } from '@ant-design/icons-vue'
+</script>
+<template>
+  <div class="mx-auto w-1/2 text-center">
+    <img alt="403 Forbidden" title="403 Forbidden" :src="err403" />
+    <a-button class="btn-has-icon" href="/" type="primary" size="large">
+      {{ t('返回首页') }}
+      <HomeOutlined />
+    </a-button>
+  </div>
+</template>
--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -1,1 +1,14 @@
-<template><div>404</div></template>
+<script setup lang="ts">
+import err404 from '@/assets/404 NotFound.png'
+import { t } from '@/utils/i18n'
+import { HomeOutlined } from '@ant-design/icons-vue'
+</script>
+<template>
+  <div class="mx-auto w-1/2 text-center">
+    <img alt="404 Not Found" title="404 Not Found" :src="err404" />
+    <a-button class="btn-has-icon" href="/" type="primary" size="large">
+      {{ t('返回首页') }}
+      <HomeOutlined />
+    </a-button>
+  </div>
+</template>
